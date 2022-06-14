239c4a4e1db2585eb3a9b7d6d505d8d42a52bfa0
Dhiraj Office <dhirajlucidux.com>
2022-06-14 17:30:59 +0530

    added map with autofill address

--- src/app/postcode-fill-auto/postcode-fill-auto.component.ts
+++ src/app/postcode-fill-auto/postcode-fill-auto.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 declare var clickToAddress: any;
 declare var $: any;
-
+declare var google:any;
 
 @Component({
   selector: 'app-postcode-fill-auto',
@@ -21,12 +21,16 @@ export class PostcodeFillAutoComponent implements OnInit {
     'county': '',
     'country': '',
     'unitno': '',
+    'lat': '',
+    'long': '',
+
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.fillAutoPostcode();
+    this.loadMap();
   }
 
 
@@ -73,5 +77,90 @@ export class PostcodeFillAutoComponent implements OnInit {
     console.log(this.myform.value);
 
   }
+  map:any;
+
+  loadMap(){
+    let myLatlng = new google.maps.LatLng(51.507351, -0.127758);
+
+      let myOptions = {
+          zoom: 5,
+          center: myLatlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      this.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+      var marker = new google.maps.Marker({
+          draggable: true,
+          position: myLatlng,
+          map: this.map,
+          title: "Your location"
+      });
+
+      this.getPostion(marker);
+
+  }
+
+  getPostion(marker:any){
+  let infoWindow = new google.maps.InfoWindow;
+    
+    google.maps.event.addListener(marker, 'dragend',  (event:any)=> {
+      
+      this.myform.patchValue({
+        "lat":event.latLng.lat(),
+        "long":event.latLng.lng()
+      })
+        
+        let Latlng = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+        let geocoder = new google.maps.Geocoder();
+        infoWindow.open(this.map, marker);
+        geocoder.geocode({
+          latLng: Latlng
+        }, (responses:any)=> {
+          if (responses && responses.length > 0) {
+            infoWindow.setContent(responses[0].formatted_address);
+            let addr = responses[0].formatted_address;
+            addr = addr.split(',');
+            let addr1 = addr[0].split(' ');
+            if(addr1.length > 1){
+              let len = addr1.length;
+              let streetAddr = '';
+              for(let i=1;i<len;i++){
+                streetAddr += addr1[i];
+              }
+
+
+              this.myform.patchValue({
+                "Address":addr1[0],
+                "street_name":streetAddr
+              })
+
+            }else{
+              this.myform.patchValue({
+                "Address":addr1[0]
+              })
+
+            }
+            
+            this.myform.patchValue({
+              "Address2":addr[1]
+            })
+            
+            if(addr[addr.length-1] == "UK"){
+              this.myform.patchValue({
+                "Address":addr1[0]
+              })
+            }else{
+              this.myform.patchValue({
+                "country":'United Kingdom',
+                "town":addr[addr.length-2]
+              })
+            }
+          } else {
+            infoWindow.setContent('Cannot determine address at this location.');
+          }
+        });
+        
+    });
+}
 
 }
